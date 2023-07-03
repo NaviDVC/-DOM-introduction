d26c5d38ba3a0266500ea3f9c2b36c5f267c01e4
NaviDVC <ivan999az.vf@gmail.com>
2023-07-03 20:39:37 +0300

    images task done

--- main.js
+++ main.js
@@ -50,3 +50,13 @@ function createTable() {
 
     elem.appendChild(table);
 }
+
+// 4. У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. Вивести зображення з цієї папки отримане випадковим чином (Math.random)
+
+const imagesArray = new Array('images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg')
+
+function randomPicture() {
+    const randomNum = Math.floor(Math.random() * imagesArray.length)
+    document.getElementById('anyPicture').src = imagesArray[randomNum];
+}
+window.onload = randomPicture();
